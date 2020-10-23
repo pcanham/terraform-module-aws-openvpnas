55556787af3e1a08f5c46071c290c72dd9c5ccd7
Paul Canham <paul@canham.io>
2020-10-23 14:47:21 +0100

    brain dump of skeleton code and notes.

--- iam.tf
+++ iam.tf
@@ -7,12 +7,17 @@ resource "aws_iam_role" "openvpn" {
   "Statement": [ { "Effect": "Allow", "Principal": { "Service": "ec2.amazonaws.com" }, "Action": "sts:AssumeRole" } ]
 }
 EOF
-  tags = var.tags
+  tags = merge(
+    local.common_tags,
+    {
+      "SERVICE" = "IAM"
+    }
+  )
 }
 
 resource "aws_iam_role_policy" "openvpn" {
   count = var.create_openvpnas ? 1 : 0
-  name = "openvpn"
+  name = "openvpn_r53"
   role = aws_iam_role.openvpn[0].id
   policy = <<EOF
 {
@@ -24,6 +29,46 @@ resource "aws_iam_role_policy" "openvpn" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_policy" "ssm_s3_access" {
+  name   = "openvpnas_s3_ssm_access"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+  "Effect": "Allow",
+  "Action": [
+    "s3:GetBucketLocation",
+    "s3:PutObject",
+    "s3:GetObject",
+    "s3:GetEncryptionConfiguration",
+    "s3:AbortMultipartUpload",
+    "s3:ListMultipartUploadParts",
+    "s3:ListBucket",
+    "s3:ListBucketMultipartUploads"
+  ],
+  "Resource": "*"
+  }
+]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_s3_policy_attachment" {
+  role       = aws_iam_role.openvpn[0].name
+  policy_arn = aws_iam_policy.ssm_s3_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_role_policy01" {
+  role       = aws_iam_role.openvpn[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_role_policy02" {
+  role       = aws_iam_role.openvpn[0].name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_instance_profile" "openvpn" {
