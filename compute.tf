4f010fee78cbb2b217d13784358c867fd157d004
Paul Canham <paul@canham.io>
2020-12-08 09:50:36 +0000

    no indices setup within this variable

--- compute.tf
+++ compute.tf
@@ -38,7 +38,7 @@ resource "aws_instance" "openvpn" {
   ami                    = var.ami_id == "" ? data.aws_ami.openvpn.image_id : var.ami_id
   instance_type          = var.instance_type
   key_name               = var.ssh_key == "" ? null : var.ssh_key
-  subnet_id              = var.public_subnet_id[0]
+  subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.openvpn.id]
   iam_instance_profile   = aws_iam_instance_profile.openvpn.name
   #tfsec:ignore:AWS012
