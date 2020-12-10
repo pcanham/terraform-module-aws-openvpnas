6b171c174dd7e90e035613d889e90fee4ec4e863
Paul Canham <paul@canham.io>
2020-12-10 15:45:01 +0000

    Check is only doing a dry run and not installing anything
    this means that pip is not being installed and fails the playbook due to the pip dependancy

--- ssm.tf
+++ ssm.tf
@@ -80,7 +80,7 @@ resource "aws_ssm_association" "openvpnas" {
     )) })
     PlaybookFile        = "main.yml"
     InstallDependencies = "True"
-    Check               = "True"
+    Check               = "False"
     Verbose             = "-v"
   }
   output_location {
