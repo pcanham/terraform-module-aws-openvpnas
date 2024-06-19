f5b8a72b06e4ca0af894385200a602bfeae060db
pscanham <paul@canham.io>
2024-06-19 13:07:45 +0000

    chore: rework createvpc script

--- scripts/CreateDefaultVpc.py
+++ scripts/CreateDefaultVpc.py
@@ -16,7 +16,7 @@ vpc_client = boto3.client("ec2", region_name=AWS_REGION, endpoint_url=ENDPOINT_U
 
 def create_default_vpc():
     """
-    Creates a default VPC in the configured availability zone.
+    Creates a default VPC in the configured region.
     """
     try:
         response = vpc_client.create_default_vpc()
@@ -24,7 +24,7 @@ def create_default_vpc():
         logger.exception("Could not create default vpc.")
         pass
     else:
-        return response
+        return response['Vpc']
 
 
 def check_for_vpc(vpc_client):
@@ -37,18 +37,15 @@ def check_for_vpc(vpc_client):
     return len(vpcs) > 0
 
 
-if __name__ == "__main__":
-    if has_default_vpc(vpc_client):
-        logger.info("Has default VPC. Skipping creation.")
-        response = vpc_client.describe_vpcs(
-            Filters=[
-                {"Name": "isDefault", "Values": ["true"]},
-            ]
-        )
-        return default_vpc["Vpc"]["VpcId"]
+def create_default_vpc_if_not_exist(vpc_client):
+    if check_for_vpc(vpc_client):
+        logger.info("Has default VPC. Skipping create.")
+        return
 
-    logger.info("Creating default VPC...")
-    default_vpc = create_default_vpc()
-    logger.info(
-        f"Default VPC is created with VPC ID: {default_vpc["Vpc"]["VpcId"]}"
-    )
+    logger.info("Does not have default VPC. Creating.")
+    vpc = create_default_vpc(vpc_client)
+    logger.info("Created VPC {vpc['VpcId']}")
+
+
+if __name__ == "__main__":
+    create_default_vpc_if_not_exist(vpc_client)
