030202f82323495577bb55ce91121382a1057f18
Paul Canham <paul@canham.io>
2024-06-20 22:02:12 +0100

    chore(lint): [skip ci] whitespace fixes

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-    suffix = coalesce(var.custom_suffix, random_id.suffix.id)
+  suffix = coalesce(var.custom_suffix, random_id.suffix.id)
 }
--- scripts/main.tf
+++ scripts/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region                   = var.aws_region
-  }
+  region = var.aws_region
+}
 
 module "network" {
   source                = "git::https://github.com/pcanham/terraform-module-aws-vpc.git?ref=0.1.26"
@@ -13,10 +13,10 @@ module "network" {
   private_cidr_blocks01 = var.private_cidr_blocks01
   private_cidr_blocks02 = var.private_cidr_blocks02
   private_cidr_blocks03 = var.private_cidr_blocks03
-  public_tier_name = var.public_tier_name
-  private01_tier_name = var.private01_tier_name
-  private02_tier_name = var.private02_tier_name
-  private03_tier_name = var.private03_tier_name
+  public_tier_name      = var.public_tier_name
+  private01_tier_name   = var.private01_tier_name
+  private02_tier_name   = var.private02_tier_name
+  private03_tier_name   = var.private03_tier_name
 
   tags = merge(
     var.tags,
