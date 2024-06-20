5d8d3868e519fb537e41af64a2ae78023512f22e
pscanham <paul@canham.io>
2024-06-20 13:07:50 +0000

    fix: deprecated setting for server side encryption

--- s3.tf
+++ s3.tf
@@ -9,11 +9,14 @@ resource "aws_s3_bucket" "ansible_bucket" {
       "SERVICE" = "STORAGE"
     }
   )
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ansible_bucket" {
+  bucket = aws_s3_bucket.ansible_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
